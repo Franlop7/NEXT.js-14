4268c3d681bcf23003fff8658ca6bd8d44f14ed3
Fran Lopez <franciscolopto@gmail.com>
2023-11-18 18:25:38 +0100

    add image mobile

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -42,10 +42,10 @@ export default function Page() {
           />
           <Image
             src="/hero-mobile.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
       </div>
